55abf46504ff9794b115aedda76b775f1e899ae2
Asif-Abdullah01 <asifabdullah214365@gmail.com>
2025-01-09 12:44:02 +0600

    specific users food get added

--- index.js
+++ index.js
@@ -29,15 +29,15 @@ async function run() {
 
     //add a single food item
 
-    app.post('/add-food',async(req,res)=> {
-        const foodData = req.body;
-        const result = await foodsCollection.insertOne(foodData)
-       
-        res.send(result)
+    app.post('/add-food', async (req, res) => {
+      const foodData = req.body;
+      const result = await foodsCollection.insertOne(foodData)
+
+      res.send(result)
     })
 
     //add a single order to orders
-    app.post('/add-order',async(req,res)=> {
+    app.post('/add-order', async (req, res) => {
       const orderedData = req.body;
 
       const result = await ordersCollection.insertOne(orderedData)
@@ -46,27 +46,39 @@ async function run() {
 
 
     //get all foods data
-    app.get('/foods',async(req,res)=> {
+    app.get('/foods', async (req, res) => {
       const result = await foodsCollection.find().toArray();
       res.send(result);
     })
 
-    app.get('/foods/:id',async(req,res)=> {
+    //get a specific food details
+    app.get('/foods/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
 
       const result = await foodsCollection.findOne(query)
       res.send(result);
     })
 
-    app.get('/foods/purchase/:id',async(req,res)=> {
+    //get a specific food details for purchase
+    app.get('/foods/purchase/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
 
       const result = await foodsCollection.findOne(query)
       res.send(result);
     })
 
+    // get all foods posted by a specific user
+    app.get('/food/:email', async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+
+      const query = { 'addBy.email': email }
+      const result = await foodsCollection.find(query).toArray();
+      // console.log(result);
+      res.send(result)
+    })
 
 
 
@@ -83,10 +95,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=> {
-    res.send('Your food is preparing....')
+app.get('/', (req, res) => {
+  res.send('Your food is preparing....')
 })
 
-app.listen(port,()=> {
-    console.log(`Food is preparing at: ${port}`);
+app.listen(port, () => {
+  console.log(`Food is preparing at: ${port}`);
 })
